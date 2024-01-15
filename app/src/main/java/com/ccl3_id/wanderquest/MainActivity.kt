ac4ae09a1ddbbdd48a6c60bdd4cf31873efa561d
LetsBigSnack <83255386+LetsBigSnack@users.noreply.github.com>
2024-01-15 17:19:53 +0100

    Test Commit

--- app/src/main/java/com/ccl3_id/wanderquest/MainActivity.kt
+++ app/src/main/java/com/ccl3_id/wanderquest/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             WanderQuestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Igor")
                 }
             }
         }
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WanderQuestTheme {
-        Greeting("Android")
+        Greeting("Igor")
     }
 }
