72fd6f9975c4a76daac9194f212a311c57a4b33b
LetsBigSnack <83255386+LetsBigSnack@users.noreply.github.com>
2024-01-18 12:56:17 +0100

    Added LocationTrackingService

--- app/src/main/java/com/ccl3_id/wanderquest/LoginActivity.kt
+++ app/src/main/java/com/ccl3_id/wanderquest/LoginActivity.kt
@@ -1,18 +1,24 @@
 package com.ccl3_id.wanderquest
 
 import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.ccl3_id.wanderquest.data.DatabaseHandler
 import com.ccl3_id.wanderquest.handlers.PermissionHandler
+import com.ccl3_id.wanderquest.services.LocationTrackingService
 import com.ccl3_id.wanderquest.ui.theme.WanderQuestTheme
 import com.ccl3_id.wanderquest.ui.views.LoginView
 import com.ccl3_id.wanderquest.viewModels.LoginViewModel
+import android.provider.Settings
 
 
 class LoginActivity : ComponentActivity() {
@@ -23,7 +29,8 @@ class LoginActivity : ComponentActivity() {
     private val requiredPermissions = arrayOf(
         Manifest.permission.ACTIVITY_RECOGNITION,
         Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.FOREGROUND_SERVICE
+        Manifest.permission.FOREGROUND_SERVICE,
+        Manifest.permission.POST_NOTIFICATIONS
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +38,7 @@ class LoginActivity : ComponentActivity() {
 
         if (permissionHandler.hasPermissions(requiredPermissions)) {
             println("Permission Granted")
+            startLocationTrackingService()
         } else {
             println("Ask Permission")
             permissionHandler.requestPermissions(requiredPermissions)
@@ -53,9 +61,19 @@ class LoginActivity : ComponentActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (permissionHandler.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            startLocationTrackingService()
             println("Permission Granted")
         } else {
             println("Denied")
         }
     }
+
+    private fun startLocationTrackingService() {
+        val intent = Intent(this, LocationTrackingService::class.java).apply {
+            action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+            putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        }
+        startService(intent)
+    }
+
 }
---
+++ app/src/main/java/com/ccl3_id/wanderquest/services/LocationTrackingService.kt
@@ -0,0 +1,73 @@
+package com.ccl3_id.wanderquest.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.ccl3_id.wanderquest.R
+
+class LocationTrackingService : Service(), LocationListener {
+    private lateinit var locationManager: LocationManager
+    private var lastLocation: Location? = null
+    private var distanceWalked: Float = 0f
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        println("LocationTrackingService Started")
+        super.onCreate()
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1f, this)
+        } catch (e: SecurityException) {
+            println("Permission not Granted")
+            // Handle exception (permissions not granted)
+        }
+        startForegroundService()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun startForegroundService() {
+        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel("location_service_channel", "Location Service Channel")
+        } else {
+            ""
+        }
+
+        val notification: Notification = Notification.Builder(this, channelId)
+            .setContentTitle("Location Service")
+            .setContentText("Tracking your location")
+            .setSmallIcon(R.drawable.cap)
+            .build()
+
+        startForeground(1, notification)
+    }
+
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW)
+            val service = getSystemService(NotificationManager::class.java)
+            service.createNotificationChannel(channel)
+        }
+        return channelId
+    }
+
+    override fun onLocationChanged(location: Location) {
+        println(distanceWalked)
+        lastLocation?.let {
+            distanceWalked += it.distanceTo(location)
+        }
+        lastLocation = location
+        // You might want to communicate this distance to the UI via a LiveData or similar mechanism
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
